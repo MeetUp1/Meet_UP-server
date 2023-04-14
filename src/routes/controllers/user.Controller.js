81fcf15211756ca44b2cca71f94edc3413bf9ecf
HyukE <ming2ki2@gmail.com>
2023-04-15 04:10:47 +0800

    feat: createMeeting end point 구현

--- src/routes/controllers/user.Controller.js
+++ src/routes/controllers/user.Controller.js
@@ -22,3 +22,32 @@ exports.loginUser = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.patchOpenTime = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    console.log(id);
+    const user = await User.findOne({ id });
+    console.log(user);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    user.openTime = req.body.selectedDateTime;
+    await user.save();
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getOpenTime = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findOne({ id });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send(user);
+  } catch (err) {
+    next(err);
+  }
+};
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const userController = require("./controllers/user.Controller");
 
 router.post("/login", userController.loginUser);
+router.patch("/:id", userController.patchOpenTime);
+router.get("/:id", userController.getOpenTime);
 
 module.exports = router;
